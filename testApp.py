a62e708b8c8f78eee338386070106afe8536b373
AndrewMerrow <48524719+AndrewMerrow@users.noreply.github.com>
2023-06-09 09:12:39 -0500

    initial commit

---
+++ testApp.py
@@ -0,0 +1,24 @@
+# how to use css in python_ flask
+# flask render_template example
+ 
+from flask import Flask, render_template
+ 
+# WSGI Application
+# Provide template folder name
+# The default folder name should be "templates" else need to mention custom folder name
+app = Flask(__name__, template_folder='templates', static_folder='staticFiles')
+ 
+# @app.route('/')
+# def welcome():
+#     return "This is the home page of Flask Application"
+ 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/groups', strict_slashes=False, methods=["GET"])
+def groups():
+    return render_template('groupsPage.html')
+ 
+if __name__=='__main__':
+    app.run(debug = True)
